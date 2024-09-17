7dd5d0166d733658b8234bc0be79ef1ce707af53
CoffeesLia <98719299+httpscecili@users.noreply.github.com>
2024-09-17 11:47:43 -0300

    Create YouInDogYears.js

---
+++ YouInDogYears.js
@@ -0,0 +1,22 @@
+const myAge = 'YOUR AGE';
+// a variable just like your age
+
+let earlyYears = 2;
+// another variable just with the value '2'
+
+earlyYears *= 10.5;
+
+let laterYears = myAge - 2;
+/* just multiply the earlyYears variable, and make an equation with myAge one with the oldest value of the first mentioned variable, turned this result into a new variable. */
+
+laterYears *= 4;
+// actual value of laterYears variable
+
+let myAgeInDogYears = earlyYears + laterYears;
+// this is the value of your age in dog years
+
+let myName = 'YOUR NAME'.toLowerCase();
+// here you assign your name to 'YOUR NAME' in lowercase
+
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`);
+// that displays your name and age in dog years
